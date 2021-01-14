6a796d26e56a6af8237c4b2daeb349fd49f630cf
Yamen <osaretinosayamen@outlook.com>
2021-01-14 23:24:30 +0100

    add api response to weather data fetch error

--- chemondis/weather/utils/weather.py
+++ chemondis/weather/utils/weather.py
@@ -1,10 +1,11 @@
 import aiohttp
 from .exceptions import WeatherDataException
 
+
 class WeatherData:
     def __init__(self, weather_params):
         self.params = weather_params
-    
+
     async def __aenter__(self):
         weather_data = await self.get_weather_data()
         self.weather_data = weather_data
@@ -13,21 +14,27 @@ class WeatherData:
     async def get_weather_data(self):
         async with aiohttp.ClientSession() as session:
             params = self.params
-            city = params['city']
-            appId = params['appId']
-            units = params.get('units', 'metric')
-            lang = params.get('lang', 'en')
+            city = params["city"]
+            appId = params["appId"]
+            units = params.get("units", "metric")
+            lang = params.get("lang", "en")
 
             try:
-                async with session.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units={}&lang={}'.format(city, appId, units, lang)) as response:
+                async with session.get(
+                    "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units={}&lang={}".format(
+                        city, appId, units, lang
+                    )
+                ) as response:
                     weatherData = await response.json()
-                    if (weatherData.get('cod') != 200):
-                        raise WeatherDataException(message=weatherData.get('message'))
+                    if weatherData.get("cod") != 200:
+                        raise WeatherDataException(message=weatherData.get("message"))
                     return weatherData
             except Exception as exc:
                 # TODO: Log exceptions in api call
-                raise WeatherDataException(message='Error fetching weather information')
-    
+                raise WeatherDataException(
+                    message="Error fetching weather information: {}".format(exc.message)
+                )
+
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         # handle caching the city information here
-        pass  
+        pass
