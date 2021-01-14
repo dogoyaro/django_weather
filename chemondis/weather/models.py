63fbd171c3b0ed454b536a476dbb09b18eb91aa7
Yamen <osaretinosayamen@outlook.com>
2021-01-14 22:46:36 +0100

    integrate weather data context manage to weather views

--- chemondis/weather/models.py
+++ chemondis/weather/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Weather(models.Model):
+    temp = models.FloatField()
+    max_temp = models.FloatField()
+    min_temp = models.FloatField()
+    humidity = models.FloatField()
+    windspeed = models.FloatField()
+    windspeed_direction = models.FloatField()
+    city = models.CharField(max_length=200)
+    units = models.CharField(max_length=100)
+    lang = models.CharField(max_length=200)
+ 
+
+    def get_weather(self):
+        return {
+            'temp': self.temp,
+            'max_temp': self.max_temp,
+            'min_temp': self.min_temp,
+            'humidity': self.humidity,
+            'windspeed': self.windspeed,
+            'city': self.city,
+            'units': self.units,
+            'lang': self.lang,
+        }
--- chemondis/weather/views.py
+++ chemondis/weather/views.py
@@ -1,12 +1,33 @@
+import aiohttp
+import asyncio
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+
 from .form import CityForm
-
-
-def index(request):
+from .utils.weather import WeatherData
+from .utils.helpers import get_app_Id, get_weather_data
+from .utils.exceptions import WeatherDataException
+        
+async def index(request):
     form = CityForm(request.GET);
+    appId = get_app_Id()
 
     if not form.is_valid():
         form = CityForm()
 
-    return render(request, 'weather/index.html', { 'form': form })
+    else:
+        city = form.cleaned_data['city']
+        weather_params = {
+            'city': 'coronada',
+            'appId': '',
+        }
+        response = {'form': form}
+        try:
+            async with WeatherData(weather_params) as weather_data:
+                weather = get_weather_data(weather_data)
+                response['weather'] = weather
+        except WeatherDataException as exc:
+            error = { 'message': exc.message }
+            response['error'] = error
+
+    return render(request, 'weather/index.html', response)
