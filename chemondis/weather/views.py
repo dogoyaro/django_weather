c0a8a998b67e7e3b133788a5e8b4c4e723cd5993
Yamen <osaretinosayamen@outlook.com>
2021-01-14 13:07:52 +0100

    add city form

--- chemondis/weather/views.py
+++ chemondis/weather/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .form import CityForm
 
-# Create your views here.
+
+def index(request):
+    form = CityForm(request.GET);
+
+    if not form.is_valid():
+        form = CityForm()
+
+    return render(request, 'weather/index.html', { 'form': form })
