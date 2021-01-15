22df72ed3ad08c110df8a80e458cf636f12e1f6a
Yamen <osaretinosayamen@outlook.com>
2021-01-15 15:34:37 +0100

    add test for weather data context manager

---
+++ chemondis/weather/tests/test_utils.py
@@ -0,0 +1,78 @@
+from django.test import TestCase
+from weather.utils.weather import WeatherData
+from unittest.mock import patch
+from contextlib import asynccontextmanager
+
+test_weather = {
+    "coord": {"lon": 54.3667, "lat": 24.4667},
+    "weather": [
+        {"id": 800, "main": "Clear", "description": "clear sky", "icon": "01d"}
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 21,
+        "feels_like": 19.72,
+        "temp_min": 21,
+        "temp_max": 21,
+        "pressure": 1014,
+        "humidity": 64,
+    },
+    "visibility": 10000,
+    "wind": {"speed": 3.6, "deg": 300},
+    "clouds": {"all": 0},
+    "dt": 1610718163,
+    "sys": {
+        "type": 1,
+        "id": 7533,
+        "country": "AE",
+        "sunrise": 1610680093,
+        "sunset": 1610718912,
+    },
+    "timezone": 14400,
+    "id": 292968,
+    "name": "Abu Dhabi",
+    "cod": 200,
+}
+
+
+class MockResponse:
+    # mock json() method always returns a specific testing dictionary
+    @staticmethod
+    async def json():
+        return test_weather
+
+
+class MockSession:
+    def _init__(self, url):
+        self._text = url
+        self.status = status
+
+    @asynccontextmanager
+    async def get(self, url):
+        yield MockResponse()
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+    async def __aenter__(self):
+        return self
+
+
+class UtilsTest(TestCase):
+    @patch("weather.utils.weather.aiohttp.ClientSession")
+    async def test_get_weather_data(self, mock_session):
+        mock_session.return_value = MockSession()
+        async with WeatherData("dubai", "test_app_id", {}) as weather:
+            self.assertCountEqual(
+                weather.keys(),
+                [
+                    "temp",
+                    "min_temp",
+                    "max_temp",
+                    "humidity",
+                    "city",
+                    "description",
+                    "windspeed",
+                    "windspeed_direction",
+                ],
+            )
