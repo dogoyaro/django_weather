f29a2a9277c30aefe6d679c46df905cee7114286
Yamen <osaretinosayamen@outlook.com>
2021-01-15 15:35:31 +0100

    update weather model

--- chemondis/weather/models.py
+++ chemondis/weather/models.py
@@ -1,34 +1,11 @@
 from django.db import models
 
-class Weather(models.Model):
-    temp = models.FloatField()
-    max_temp = models.FloatField()
-    min_temp = models.FloatField()
-    humidity = models.FloatField()
-    windspeed = models.FloatField()
-    windspeed_direction = models.FloatField()
-    city = models.CharField(max_length=200)
-    units = models.CharField(max_length=100)
-    lang = models.CharField(max_length=200)
- 
+class Weather:
     def __init__(self, weatherData):
         self.weather_data = weatherData;
 
-    def get_weather(self):
-        return {
-            'temp': self.temp,
-            'max_temp': self.max_temp,
-            'min_temp': self.min_temp,
-            'humidity': self.humidity,
-            'windspeed': self.windspeed,
-            'city': self.city,
-            'units': self.units,
-            'lang': self.lang,
-        }
-
     def get_data(self):
         weather_data = self.weather_data
-        print('The main weather data', weather_data)
         main = weather_data['main']
         wind = weather_data['wind']
         weath = weather_data['weather'][0]
